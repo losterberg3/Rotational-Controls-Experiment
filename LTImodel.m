ef426219928115a249f0baaac3915fc187bb16ad
losterberg3 <losterberg@ucsd.edu>
2025-04-15 11:53:00 -0700

    Update LTImodel.m

--- LTImodel.m
+++ LTImodel.m
@@ -1,8 +1,10 @@
-%% step response for disk 1, holding disk 2
+% LTI model uses m, d, & k parameters found by analyzing the experimental data, so we are verifying these parameters in the code below
+
+%% below is for experiment 1, where we found the step response for disk 1 when holding disk 2
 
 syms s
 
-M1=m1(1,1);
+M1=m1(1,1); % these are parameters found from the experimental data
 D1=d1(1,1);
 K2=k2(1,1);
 U=0.75;
@@ -19,16 +21,19 @@ T1=linspace(0,2.5,250);
 
 f11=f1num(T1);
 
+% plotting experimental data with the LTI model
+
 figure
 hold on
 plot(T1, f11,'r')
-plot(t_received(1,2225:4400)-2.5,cart1pos(1,2225:4400),'b')
+plot(t_received(1,2225:4400)-2.5,cart1pos(1,2225:4400),'b') % cart1pos is the encoder measurement of disk 1
 legend('LTI model','Experiment')
+title('Experiment 1')
 xlabel('Time (s)')
 ylabel('Position (rad)')
 hold off
 
-%% impulse response for disk 2, holding disk 1
+%% below is for experiment 2, the impulse response for disk 2 when holding disk 1
 
 M2=m2(1,2);
 D2=d2(1,2);
@@ -48,11 +53,14 @@ T2=linspace(0,10,1000);
 
 f22=f2num(T2);
 
+% plotting experimental data with the LTI model
+
 figure
 hold on
 plot(T2-0.1, f22)
-plot(t_received(1,1:8000),(cart2pos(1,1:8000)-cart2pos(1,8000)))
+plot(t_received(1,1:8000),(cart2pos(1,1:8000)-cart2pos(1,8000))) % cart2pos is encoder measurement of disk 2
 legend('LTI model','Experiment')
+title('Experiment 2')
 xlabel('Time (s)')
 ylabel('Position (rad)')
 hold off
